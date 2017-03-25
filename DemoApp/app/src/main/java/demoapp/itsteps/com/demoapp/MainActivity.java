8c3e52fff477013480a2434f15fedc8fa4f28fc9
Deniz <deniz.hashimov@mentormate.com>
2017-03-25 14:41:24 +0200

    Added changes from second block

--- DemoApp/app/src/main/java/demoapp/itsteps/com/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/demoapp/itsteps/com/demoapp/MainActivity.java
@@ -1,6 +1,7 @@
 package demoapp.itsteps.com.demoapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,7 @@ public class MainActivity extends Activity {
 
     private TextView txtHellow;
     private Button btnClick;
+    private Button btnNext;
     private int counter = 0;
 
 
@@ -30,7 +32,21 @@ public class MainActivity extends Activity {
                 counter++;
             }
         });
+        btnNext = (Button) findViewById(R.id.btn_next);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(MainActivity.this , SecondActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
+
+    //Used when activity is started for result
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
